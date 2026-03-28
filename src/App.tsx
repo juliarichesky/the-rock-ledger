45e4fe97736c99dd54f5a66f89f095e86e26402f
julia :) <juliavaleriogs@gmail.com>
2026-03-28 20:43:20 -0300

    feat: create responsive Header with Lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import Header from './components/Header/Header';
 import './index.css'
 
 const App = () => {
   return(
     <>
-    <div className="h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-red-400 uppercase">
-        The Rock Ledger 🎸
-      </h1>
-    </div>
+    <Header />
     </>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import { Search, Guitar, Menu } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="bg-zinc-950/90 backdrop-blur-md border-b border-zinc-800 sticky top-0 z-50">
+      <div className="max-w-[1300px] mx-auto px-4 h-16 md:h-20 flex items-center justify-between">
+
+        {/* menu mobile - 480px/sm) */}
+        <button className="sm:hidden text-zinc-100 hover:text-red-500  hover:scale-110 transition-all p-1">
+          <Menu size={24} />
+        </button>
+
+        {/* logo ajuste mobile */}
+        <div className="flex items-center gap-2 group cursor-pointer">
+          <div className="bg-zinc-800 p-2 rounded-lg border border-zinc-700 group-hover:border-red-600 transition-all">
+            <Guitar className="w-5 h-5 md:w-6 md:h-6 text-red-500" />
+          </div>
+          <h1 className="text-lg md:text-xl font-black uppercase tracking-normal text-zinc-100">
+            The Rock <span className="text-red-600">Ledger</span>
+          </h1>
+        </div>
+
+        {/* navbar - escondida no mobile, aparece a partir do breakpoint (480px) */}
+        <nav className="hidden sm:block">
+          <ul className="flex gap-4 md:gap-8 text-[10px] md:text-xs font-bold uppercase tracking-[0.2em] text-zinc-400">
+            <li className="hover:text-red-500 cursor-pointer transition-colors">
+              Home
+            </li>
+            <li className="hover:text-red-500 cursor-pointer transition-colors">
+              Genres
+            </li>
+            <li className="hover:text-red-500 cursor-pointer transition-colors">
+              Contact
+            </li>
+          </ul>
+        </nav>
+
+        {/* busca */}
+        <button className="text-zinc-300 hover:text-red-500 hover:scale-110 transition-all p-2">
+          <Search size={20} />
+        </button>
+      </div>
+    </header>
+  );
+}
